5e2f9536df77a4023461f88d5c5916cf3bf70af9
Sheva Vulakh <Sheva@HON-2016532.local>
2022-12-13 23:54:14 +0200

    m6

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -5,39 +5,39 @@ import { useAuth } from './User.js';
 
 function Profile() {
 const {handleNewUserName,handleUserNameInput,newUserName,user,isLoading} = useContext(SomeContext)
-const {upload} = useAuth();
-const [photoURL,setPhotoURL] = useState();
-const [photo,setPhoto] = useState(null);
-const handlePic = (e)=>{
-  if (e.target.files[0]){
-    setPhoto(e.target.files[0]);
-  }
-}
-  const handleClick = () =>{
-    upload(photo,user);
-  }
-  useEffect(()=>{
-    if(user&&user.photoURL){
-      setPhotoURL(user.photoURL);
-    }
-  },[user])
+// const {upload} = useAuth();
+// const [photoURL,setPhotoURL] = useState('avataaars.svg');
+// const [photo,setPhoto] = useState(null);
+// const handlePic = (e)=>{
+//   if (e.target.files[0]){
+//     setPhoto(e.target.files[0]);
+//   }
+// }
+//   const handleClick = () =>{
+//     upload(photo,user);
+//   }
+//   useEffect(()=>{
+//     if(user&&user.photoURL){
+//       setPhotoURL(user.photoURL);
+//     }
+//   },[user])
 
   return (
     <div className='profile'>
       <h1>Profile</h1>
       <div className='picContainer'>
-        <img src={photoURL} alt ='avatar'className="avatar-pic"></img>
+        {/* <img src={photoURL} alt ='avatar'className="avatar-pic"></img> */}
       </div>
       <h3>User Name</h3>
-      <div>{user.displayName}</div>
+      {/* <div>{user.displayName}</div> */}
       <form className="profileForm" onSubmit={handleNewUserName}>
         <input className="loginInput" onChange={handleUserNameInput} value={newUserName}/>
         <button className="profileButton"type="submit">Save</button>
       </form>
-      <div className="picFile">
+      {/* <div className="picFile">
           <input type ={'file'} onChange={handlePic}></input>
           <Button onClick={handleClick}></Button>
-        </div>
+        </div> */}
      
     </div>
   )
